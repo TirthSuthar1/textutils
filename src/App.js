f7b265a3fb2121a7d157eb8a660cf6ace836ca62
TirthSuthar1 <realtirthsuthar@gmail.com>
2022-06-02 22:15:46 +0530

    Added Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import About from "./Components/About";
 import Navbar from "./Components/Navbar";
 import TextForm from "./Components/TextForm";
 
@@ -8,6 +9,7 @@ function App() {
       <Navbar title = 'TextUtils' about = "About TextUtils"/>
       <div className="container my-3" >
       <TextForm heading = "Enter your text here to analyze"/>
+      <About/>
       </div>
     </>
   );  
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -8,6 +8,11 @@ export default function (props) {
         setText(newText);
     }
 
+    const handleDownClick = () => {
+      let newText = text.toLowerCase();
+      setText(newText);
+    }
+
     const handleOnChange = (event) => {
         console.log("Clicked on change");
         setText(event.target.value);
@@ -15,13 +20,23 @@ export default function (props) {
     const [text, setText] = useState('Enter Text Here');
 
   return (
-    
-    <div>
+    <>
+    <div className='container'>
         <h1>{props.heading}</h1>
         <div className="mb-3">
         <textarea className="form-control" value = {text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Click me for uppercase</button>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>Click me for uppercase</button>
+        <button className="btn btn-primary mx-1" onClick={handleDownClick}>Click me for lowercase</button>
     </div>
+    <div className="container my-3">
+      <h1>Your Text Summary</h1>
+      <p>{text.split("").length} words and {text.length} characters</p>
+      <p>{0.008 * text.split("").length}Minutes to read</p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+    
+    </div>
+    </>
   )
 }
